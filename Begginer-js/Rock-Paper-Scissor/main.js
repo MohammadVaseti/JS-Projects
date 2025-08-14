9943f3dc5ea8286090e717ceccc17dfd753bc7e6
mohammad vaseti <mohammadvaseti1998@gmail.com>
2025-08-14 15:24:26 +0330

    repository updated

--- Begginer-js/Rock-Paper-Scissor/main.js
+++ Begginer-js/Rock-Paper-Scissor/main.js
@@ -1,164 +1,106 @@
+// "use strict";
+// // elements
+// const choices = ["rock", "paper", "scissors"];
 
-let rock = document.querySelector('.rock')
-let paper = document.querySelector('.paper')
-let scissor = document.querySelector('.scissor')
+// const btn = document.querySelector("button");
+// const input = document.querySelector(".input");
 
-let cmRes = document.querySelector('.cmRes')
-let youRes = document.querySelector('.youRes')
-let h1 = document.querySelector('h1')
+// let cmScore = 0;
+// let yourScore = 0;
 
-let com = document.querySelector('.com')
-let you = document.querySelector('.you')
+// btn.addEventListener("click", function () {
+//   const randNum = Math.floor(Math.random() * 3);
+//   let cmChoice = choices[randNum];
+//   console.log(cmChoice);
 
+//   if (input.value === cmChoice) {
+//     console.log("draw");
+//   }
+//   // ////////////
+//   else if (cmChoice === "rock" && input.value === "scissors") {
+//     ++cmScore;
+//     console.log("cm win", cmScore);
+//   } else if (cmChoice === "rock" && input.value === "paper") {
+//     ++yourScore;
+//     console.log("you win", yourScore);
+//   }
+//   // /////////////
+//   else if (cmChoice === "scissors" && input.value === "rock") {
+//     ++yourScore;
+//     console.log("you win", yourScore);
+//   } else if (cmChoice === "scissors" && input.value === "paper") {
+//     ++cmScore;
+//     console.log("cm win", cmScore);
+//   }
+//   // ///////////////////
+//   else if (cmChoice === "paper" && input.value === "rock") {
+//     ++cmScore;
+//     console.log("cm win", cmScore);
+//   } else if (cmChoice === "paper" && input.value === "scissors") {
+//     ++yourScore;
+//     console.log("you win", yourScore);
+//   }
+// });
 
+/////////////////////////////////////////////////////////////////////
 
+"use strict";
+// elements
+const choices = ["rock", "paper", "scissors"];
 
-let cmScore =0
+const yours = document.querySelector(".yourscore");
+const cms = document.querySelector(".cmscore");
+console.log(cms, yours);
+const btn = document.querySelector("button");
+const input = document.querySelector(".input");
 
-let youScore = 0
+// بازیابی امتیاز ذخیره‌شده یا صفر اگر وجود نداشت
+let cmScore = Number(localStorage.getItem("cmScore")) || 0;
+let yourScore = Number(localStorage.getItem("yourScore")) || 0;
 
+btn.addEventListener("click", function () {
+  const randNum = Math.floor(Math.random() * 3);
+  let cmChoice = choices[randNum];
+  console.log(cmChoice);
 
-
-
-
-
-
-
-
-
-function randNum () {
-let array = [ 'rock' , 'paper' , 'scissor']
-let num = Math.floor(Math.random()*3)
-return array[num]
-
-}
-
- let cmInput =  randNum()
-
-rock.addEventListener('click' , function () {
-  
-
-  youRes.innerHTML = 'rock'.toUpperCase()
-  cmRes.innerHTML = cmInput.toUpperCase()
-  if(cmInput == 'rock'){
-    h1.innerHTML = 'draw'
-    h1.style.backgroundColor = 'yellow'
-
-
+  if (input.value === cmChoice) {
+    console.log("draw");
   }
-  else{
-
-    if(cmInput == 'scissor' ) {
-
-      h1.innerHTML = ' win'
-      h1.style.backgroundColor = 'green'
-      youScore++ ;
-      you.innerHTML += youScore
-
-
-
-    }
-    else if (cmInput == 'paper'){
-      h1.innerHTML = 'lose'
-      h1.style.backgroundColor = 'red'
-      cmScore++
-      com.innerHTML += cmScore ;
-
-
-
-
-
-    }
+  // ////////////
+  else if (cmChoice === "rock" && input.value === "scissors") {
+    ++cmScore;
+    console.log("cm win", cmScore);
+  } else if (cmChoice === "rock" && input.value === "paper") {
+    ++yourScore;
+    console.log("you win", yourScore);
   }
-
-})
-
-
-paper.addEventListener('click' , function () {
-  youRes.innerHTML = 'paper'.toUpperCase()
-  cmRes.innerHTML = cmInput.toUpperCase()
-
-
-  your='paper'
-  if(cmInput== 'paper'){
-    h1.innerHTML = 'draw'
-    h1.style.backgroundColor = 'yellow'
-
+  // /////////////
+  else if (cmChoice === "scissors" && input.value === "rock") {
+    ++yourScore;
+    console.log("you win", yourScore);
+  } else if (cmChoice === "scissors" && input.value === "paper") {
+    ++cmScore;
+    console.log("cm win", cmScore);
   }
-  else{
-    if(cmInput == 'rock' ) {
-      h1.innerHTML = ' win'
-      h1.style.backgroundColor = 'green'
-      youScore++ ;
-      you.innerHTML += youScore
-
-
-
-
-
-    }
-    else if (cmInput == 'scissor'){
-      h1.innerHTML = 'lose'
-      h1.style.backgroundColor = 'red'
-      cmScore++
-      com.innerHTML += cmScore ;
-
-
-
-
-
-    }
+  // ///////////////////
+  else if (cmChoice === "paper" && input.value === "rock") {
+    ++cmScore;
+    console.log("cm win", cmScore);
+  } else if (cmChoice === "paper" && input.value === "scissors") {
+    ++yourScore;
+    console.log("you win", yourScore);
   }
 
-})
+  // ذخیره امتیازها در localStorage
+  localStorage.setItem("cmScore", cmScore);
+  localStorage.setItem("yourScore", yourScore);
 
-
-scissor.addEventListener('click' , function () {
-
-  youRes.innerHTML = 'scissor'.toUpperCase()
-  cmRes.innerHTML = cmInput.toUpperCase()
-
-
-  your='scissor'
-  if(cmInput=='scissor'){
-    h1.innerHTML = 'draw'
-    h1.style.backgroundColor = 'yellow'
-
-
-  }
-  else{
-    if(cmInput == 'rock' ) {
-      h1.innerHTML = 'lose'
-      h1.style.backgroundColor = 'red'
-      cmScore++
-      com.innerHTML += cmScore ;
-
-
-
-
-
-    }
-    else if (cmInput == 'paper'){
-      h1.innerHTML = 'win'
-      h1.style.backgroundColor = 'green'
-      youScore++ ;
-      you.innerHTML += youScore
-
-
-
-
-
-    }
-  }
-
-
-
-})
-
-
-
-
-
-
-
-
+  cms.innerHTML = `compter = ${cmScore}`;
+  yours.innerHTML = `you = ${yourScore}`;
+});
+// window.addEventListener("load", function () {
+//   cmScore = 0;
+//   yourScore = 0;
+//   localStorage.setItem("cmScore", cmScore);
+//   localStorage.setItem("yourScore", yourScore);
+// });
