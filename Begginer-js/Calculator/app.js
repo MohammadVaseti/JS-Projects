a637f5919f6b892494c98eb53fa88d00248e07e2
mohammad vaseti <mohammadvaseti1998@gmail.com>
2025-09-14 11:37:20 +0330

    repository updated

--- Begginer-js/Calculator/app.js
+++ Begginer-js/Calculator/app.js
@@ -6,3 +6,9 @@ console.log(btns);
 const display = document.querySelector(".display");
 console.log(display);
 // ///////////////////////
+
+btns.forEach((element) => {
+  element.addEventListener("click", (e) => {
+    display.innerHTML += e.target.textContent;
+  });
+});
