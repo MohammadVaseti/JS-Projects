ac09c90debee5a45c488d7f3048e8b642010caf9
mohammad vaseti <mohammadvaseti1998@gmail.com>
2025-09-22 14:38:26 +0330

    repository updated

--- Begginer-js/15-Weight Tracker/app.js
+++ Begginer-js/15-Weight Tracker/app.js
@@ -1,1 +1,7 @@
 "use strict";
+// elements
+const input = document.querySelector(".input");
+const btn = document.querySelector(".btn");
+const list = document.querySelector(".weight_list");
+const result = document.querySelector(".result");
+console.log(result);
