b2b1be0e08507bcdf7775a0a8dfd9b05d11a460c
mohammad vaseti <mohammadvaseti1998@gmail.com>
2025-09-27 16:38:43 +0330

    repository updated

--- Begginer-js/15-Weight Tracker/app.js
+++ Begginer-js/15-Weight Tracker/app.js
@@ -9,7 +9,7 @@ console.log(result);
 const savedWeights = JSON.parse(localStorage.getItem("weight")) || [];
 const weightArr = savedWeights;
 
-// show data
+// show datas
 savedWeights.forEach((item) => {
   const li = document.createElement("li");
   li.innerText = `Weight: ${item.weight} - Time: ${new Date(
