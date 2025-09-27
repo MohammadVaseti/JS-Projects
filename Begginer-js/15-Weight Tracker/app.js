01f9d881c17119d2c5fd3f339d636e2dd5d1b5f8
mohammad vaseti <mohammadvaseti1998@gmail.com>
2025-09-27 16:35:29 +0330

    repository updated

--- Begginer-js/15-Weight Tracker/app.js
+++ Begginer-js/15-Weight Tracker/app.js
@@ -9,6 +9,15 @@ console.log(result);
 const savedWeights = JSON.parse(localStorage.getItem("weight")) || [];
 const weightArr = savedWeights;
 
+// نمایش داده‌های ذخیره‌شده
+savedWeights.forEach((item) => {
+  const li = document.createElement("li");
+  li.innerText = `Weight: ${item.weight} - Time: ${new Date(
+    item.time
+  ).toLocaleString()}`;
+  result.appendChild(li);
+});
+
 btn.addEventListener("click", () => {
   let now = new Date();
 
@@ -18,8 +27,11 @@ btn.addEventListener("click", () => {
   const para = document.createElement("p");
   para.innerText = input.value;
   list.appendChild(para);
-  let obj = { weight: input.value, time: now };
+  let obj = { weight: input.value, time: now.getTime() };
   weightArr.push(obj);
   localStorage.setItem("weight", JSON.stringify(weightArr));
-  result.innerHTML += `weight : ${input.value} time : ${now}`;
+
+  const li = document.createElement("li");
+  li.innerText = `Weight: ${input.value} - Time: ${now.toLocaleString()}`;
+  result.appendChild(li);
 });
