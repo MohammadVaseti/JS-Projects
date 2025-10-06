857c33daa72088a0c1c65ef690941529b22b6ced
mohammad vaseti <mohammadvaseti1998@gmail.com>
2025-10-06 16:30:19 +0330

    repository updated

--- Begginer-js/16-Analog Clock/app.js
+++ Begginer-js/16-Analog Clock/app.js
@@ -3,3 +3,17 @@
 const sec = document.querySelector(".second");
 const min = document.querySelector(".minute");
 const hour = document.querySelector(".hour");
+
+setInterval(() => {
+  const time = new Date();
+  sec.style.transform = `translate(-50% , -100%) rotate(${
+    time.getSeconds() * 6
+  }deg)`;
+}, 1000);
+
+setInterval(() => {
+  const time = new Date();
+  min.style.transform = `translate(-50% , -100%) rotate(${
+    time.getMinutes() * 6 + time.getSeconds() * 0.1
+  }deg)`;
+}, 1000);
