18275b991dad5eb690d70892ba5177396ec906ac
mohammad vaseti <mohammadvaseti1998@gmail.com>
2025-10-08 17:44:25 +0330

    repository updated

--- Begginer-js/17-Recipes App/app.js
+++ Begginer-js/17-Recipes App/app.js
@@ -6,7 +6,14 @@ const result = document.querySelector(".result");
 const p = document.querySelector("p");
 
 btn.addEventListener("click", (e) => {
-  console.log(input.value);
-  p.innerHTML += input.value;
-  input.value = "";
+  if (input.value == "") {
+    alert("Fill The Input");
+  } else {
+    let url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${input.value}`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+
+    input.value = "";
+  }
 });
