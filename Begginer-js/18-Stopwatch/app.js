94fee7361a0dcd4212e2935849039215045f0094
mohammad vaseti <mohammadvaseti1998@gmail.com>
2025-10-12 14:18:22 +0330

    repository updated

--- Begginer-js/18-Stopwatch/app.js
+++ Begginer-js/18-Stopwatch/app.js
@@ -5,8 +5,22 @@ const hour = document.querySelector(".hour");
 const min = document.querySelector(".minute");
 const sec = document.querySelector(".second");
 
+const start = document.querySelector(".start");
+const pause = document.querySelector(".pause");
+const reset = document.querySelector(".reset");
+
 const hourNum = 0;
 const minNum = 0;
 const secNum = 0;
 
 // logic
+
+// reset button
+reset.addEventListener("click", (e) => {
+  hour.innerHTML = "00";
+  min.innerHTML = "00";
+  sec.innerHTML = "00";
+});
+
+// start button
+start.addEventListener;
