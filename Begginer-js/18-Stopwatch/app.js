4e2c06b48583af1dc1e251d8a9265a32be3b3465
mohammad vaseti <mohammadvaseti1998@gmail.com>
2025-10-12 11:28:41 +0330

    repository updated

---
+++ Begginer-js/18-Stopwatch/app.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// elements
+const hour = document.querySelector(".hour");
+const min = document.querySelector(".minute");
+const sec = document.querySelector(".second");
+
+const hourNum = 0;
+const minNum = 0;
+const secNum = 0;
+
+// logic
