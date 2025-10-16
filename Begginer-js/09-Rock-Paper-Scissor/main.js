c50090e140c9f0e22ed700d2ddb78437d62cc867
mohammad vaseti <mohammadvaseti1998@gmail.com>
2025-10-16 20:15:18 +0330

    repository updated

--- Begginer-js/09-Rock-Paper-Scissor/main.js
+++ Begginer-js/09-Rock-Paper-Scissor/main.js
@@ -12,30 +12,41 @@ const randomNum2 = Math.floor(Math.random() * 3);
 // random Choice
 const player1 = choices[randomNum1];
 const player2 = choices[randomNum2];
-console.log(`player1 is ${player1} player2 is ${player2}`);
+// console.log(`player1 is ${player1} player2 is ${player2}`);
 
 // game Logic
 if (player1 === player2) {
+  p1.innerHTML = `Player1 is ${player1}`;
+  p2.innerHTML = `Player2 is ${player2}`;
   res.innerHTML = "Game Was Draw";
 }
 // /////////////////
 else if (player1 == "rock" && player2 == "scissors") {
   p1.innerHTML = "player1 is Rock";
   p2.innerHTML = "player2 is Scissors";
-  res.innerHTML = "Player2 Was The Winner";
+  res.innerHTML = "Player1 Was The Winner";
 } else if (player1 == "rock" && player2 == "paper") {
-  console.log("p1 is rock p2 is paper => p2 win");
+  p1.innerHTML = "player1 is Rock";
+  p2.innerHTML = "player2 is Paper";
+  res.innerHTML = "Player2 Was The Winner";
 }
-
 // ////////////
 else if (player1 == "paper" && player2 == "scissors") {
-  console.log("p1 is paper p2 is scissors => p2 win");
+  p1.innerHTML = "player1 is Paper";
+  p2.innerHTML = "player2 is Scissors";
+  res.innerHTML = "Player2 Was The Winner";
 } else if (player1 == "paper" && player2 == "rock") {
-  console.log("p1 is paper p2 is rock => p1 win");
+  p1.innerHTML = "player1 is Paper";
+  p2.innerHTML = "player2 is Rock";
+  res.innerHTML = "Player1 Was The Winner";
 }
 // //////////////////
 else if (player1 == "scissors" && player2 == "paper") {
-  console.log("p1 is scissors p2 is paper => p1 win");
+  p1.innerHTML = "player1 is Scissors";
+  p2.innerHTML = "player2 is Paper";
+  res.innerHTML = "Player1 Was The Winner";
 } else if (player1 == "scissors" && player2 == "rock") {
-  console.log("p1 is scissors p2 is rock => p2 win");
+  p1.innerHTML = "player1 is Scissors";
+  p2.innerHTML = "player2 is Rock";
+  res.innerHTML = "Player2 Was The Winner";
 }
